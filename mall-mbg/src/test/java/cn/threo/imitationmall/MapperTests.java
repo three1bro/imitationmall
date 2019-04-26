4087f21b9352e660ba2ca8cd2f3edf2ca9ecd0cc
threo <shayean1lsy@gmail.com>
2019-04-26 15:38:03 +0800

    add description of mall-mbg

--- mall-mbg/src/test/java/cn/threo/imitationmall/MapperTests.java
+++
@@ -1,9 +0,0 @@
-package cn.threo.imitationmall;
-
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-public class MapperTests {
-
-}
